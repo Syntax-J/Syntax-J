45f82e3ea44afc419822dbe3f3b29df86fb42f86
hello <hello@qq.com>
2017-08-03 23:35:20 +0800

    modify

---
+++ javascript/mainjs.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $("#paras_1").click(function(){
+  	$('#content_title').load('./paras/paras_1.txt h1');
+    $('#total_content').load('./paras/paras_1.txt p');
+  });
+});
