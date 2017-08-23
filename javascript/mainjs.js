86a129dc1cdda4b850d97f348643ccc93c4ee8e5
hello <hello@qq.com>
2017-08-23 18:42:07 +0800

    modify

--- javascript/mainjs.js
+++ javascript/mainjs.js
@@ -3,4 +3,19 @@ $(document).ready(function(){
   	$('#content_title').load('./paras/paras_1.txt h1');
     $('#total_content').load('./paras/paras_1.txt p,a');
   });
+  $("#mainBox").velocity({
+  	opacity:"1",
+  },
+  	{
+  		duration:1000,
+  		complete:function(){
+  			$("#mainBox").velocity({
+  				opacity:"0",
+          scaleY:"0"
+  			},{
+  				duration:600,
+  				delay:1400
+  			});
+  		}
+  	});
 });
